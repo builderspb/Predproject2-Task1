606bb8ebb06e53eec2b94cfea072876f0d9e45de
anton <antonsv616@gmail.com>
2024-04-18 18:59:21 +0300

    Task1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Random;
+
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat catAge() {
+        Random random =new Random();
+        int randomAge = random.nextInt(15);
+       return new Cat(randomAge);
+
+
     }
 }
